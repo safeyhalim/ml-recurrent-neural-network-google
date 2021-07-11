e7245048c82001303b2a6f7fd6b74641a36c0b04
Safey Halim <safey.allah@gmail.com>
2021-07-11 17:31:35 +0200

    Basic setup and dependencies

---
+++ rnn.py
@@ -0,0 +1,17 @@
+# Recurernt Neural Network with LSTM to predict the opening stock prices trends of Google based on historical data
+
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+
+# Part 2 - Building the RNN (LSTM)
+
+
+
+# Part 3 - Making the predictions and visualizing the results
