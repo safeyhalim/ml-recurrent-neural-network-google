fd9a612fda32495b7ab5cf9130238b183231fc70
Safey Halim <safey.allah@gmail.com>
2021-08-08 14:53:48 +0200

    Implementing the LSTM recurrent neural network

--- rnn.py
+++ rnn.py
@@ -1,4 +1,4 @@
-# Recurernt Neural Network with LSTM to predict the opening stock prices trends of Google based on historical data
+# Recurernt Neural Network with LSTM to predict the opening stock prices *trends* (not the actual stock prices) of Google based on historical data
 
 
 # Part 1 - Data Preprocessing
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the training set
+# Importing the training set (The google stock prices of the years: 2012 to 2016)
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = dataset_train.iloc[:, 1:2].values # Create a numpy array that contains one column which
 # is the open stock price in the training data set (note: 2 is excluded but we had to put the range from
@@ -85,4 +85,53 @@ regressor.add(Dropout(0.2))
 # Since we are predicting a real value (the stock price), it's only one dimension (one neuron)
 regressor.add(Dense(units=1))
 
+# Compiling the RNN
+# A common optimizer to be used is the RMSProp optimizer. It's a advanced stochastic gradient descent optimizer, according to the Keras documentation,
+# is often a good choice for recurring neural networks
+# But in this problem, we are not using the RMSProp optimizer. Experimenting lead to the use the Adam Optimizer, it's alwasy a safe choice that lead to better results.
+# loss: since we are dealing with the regression problem, the loss will be the mean squared error (between the prediction and the real observation)
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training Set
+# epochs: 100 is a number that was experimented with and lead to good results (suitable to 5 years of training data of the google stock price)
+# batch_size: 32 a magic number that lead to good results
+regressor.fit(X_train, Y_train, epochs = 100, batch_size = 32)
+
 # Part 3 - Making the predictions and visualizing the results
+# Getting the real stock price of 2017 (from the file Google_Stock_Price_Test.csv)
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of January 2017
+# Remember that to predict the stock price of a day in January 2017, we will need the stock price of the previous 60 financial days
+# And to have these days, we will need both the training set and the testing set, because some of the days will be in the testing set, 
+# but some in the training set (because they are in the past!). Therefore, we will need some sort of concatenation. But
+# remember: the test set should never be scaled (they should be as they are). So we are going to concatenate the original 
+# data frames (read from the CSV files) of the training set and the testing set, then apply scaling to that and use it
+
+# Will contain both the training set and the testing set
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0) # concatenation along the x axis of the open stock prices of both the training set and the testing set
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60 :].values # Dataframe (not shaped as a 2D numpy array) of the inputs to the regressor which will be from 60 places before the beginning of the testing set plus the whole testing set
+# it must have the shape of observations in rows and one column
+inputs = inputs.reshape(-1, 1) # Takes the input data as rows, and creates a column a new dimension. Hence coverting a 2D numpy array. We need that shape for the fit method later on
+inputs = sc.transform(inputs) # Directly applies the transform method (without fit) because the sc (Scaling object) was already fitted to the training set above. We must apply the same scaling
+
+# Restructuring the inputs (test data) so that they also have 60 timestep for each input (like what we have done with the training data. See above)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_stock_price = regressor.predict(X_test)
+# Since the regressor is trained to predict the scaled values of the stock price, we need to inverse the scaling of the predicted output
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualizing the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price Jan 2017')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price Jan 2017')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
